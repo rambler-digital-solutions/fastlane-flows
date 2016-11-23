8e22a64e4bbed170241f3be29219f48e30b480cd
Beniamin <beniamin.sarkisyan@gmail.com>
2016-11-23 10:53:07 +0300

    Update update_automatic_codesigning.rb

--- fastlane/actions/update_automatic_codesigning.rb
+++ fastlane/actions/update_automatic_codesigning.rb
@@ -5,8 +5,8 @@ module Fastlane
         # Обновляем настройки подписи всех таргетов проекта
         setup_signing_in_xcodeproj(params[:path], params)
 
-        if Dir["Pods/Pods.xcodeproj"]
-        # Обновляем настройки таргетов в Pods.xcodeproj
+        if Dir["Pods/Pods.xcodeproj"].empty?
+          # Обновляем настройки таргетов в Pods.xcodeproj
           setup_signing_in_xcodeproj(Dir["Pods/Pods.xcodeproj"].first, params)
         end
 
